837d2ec5e5f6ff277660c3a32cb2498a616fcf03
Iulia3 <iulia_andreea9@yahoo.com>
2020-11-09 14:17:13 +0200

    +Edit

--- Controllers/BookmarksController.cs
+++ Controllers/BookmarksController.cs
@@ -53,5 +53,45 @@ namespace ArtX.Controllers
                 return View();
             }
         }
+
+        //GET: Edit
+        public ActionResult Edit(int id)
+        {
+            Bookmark bookmark = db.Bookmarks.Find(id);
+            ViewBag.Bookmark = bookmark;
+            return View();
+        }
+
+        [HttpPut]
+        public ActionResult Edit(int id, Bookmark requestBookmark)
+        {
+            try
+            {
+                Bookmark bookmark = db.Bookmarks.Find(id);
+
+                if (TryUpdateModel(bookmark))
+                {
+                    bookmark.Title = requestBookmark.Title;
+                    bookmark.Description = requestBookmark.Description;
+                    //rating ul si data nu ar trebui sa poata fi schimbate
+                    //sau de fapt logic cred ca ar fi sa se reseteze la 0 rating- ul? din moment ce se modifica content ul
+                    //sau oricum e aiurea, poate facem sa permitem doar modificarea descrierii si atat. Cum vrei si tu
+                    //vedem
+                    //iar albumul nu poate fi schimbat de aici, ca un bookmark poate fi in mai multe albume. O sa facem din AlbumController sa putem elimina/adauga un bookmark. Va fi optiunea utilizatorului
+                    //ssau poate facem si de aici.. sa apara lista albumelor in care a fost adaugat si sa putem debifa unele albume, daca ar fi sa i dam adminului permisiunea asta dar nush mai vedem, ne mai uitam pe ce zice in cerinte.
+
+                    //ramane de facut pt Content cu imagini.
+                    bookmark.Content = requestBookmark.Content;
+
+                    db.SaveChanges();
+                }
+                return RedirectToAction("Index");
+            }
+            catch (Exception e)
+            {
+                return View();
+            }
+        }
+
     }
 }
