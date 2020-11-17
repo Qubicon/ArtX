cc3766134734f14a5cb7e4c3d14053ce0c811a64
Iulia3 <iulia_andreea9@yahoo.com>
2020-11-17 11:10:44 +0200

    +New tot la Albums
    nu vad ce sa i punem  mai mult decat titlul albumului la new. Daca ar fi sa selectam si ce bookmarkuri sa contina albumul ar fi un pipc aaiurea, n am putea sa afisam toate bookmarksurile acolo, ci doar o lista cu titlurile lor si nu mi se pare practic.
    Deci eu zic ca la new pt album putem sa punem doar titlul si atat.
    O sa adaugam bookmarks la el din alta parte, direct din bookmark o sa avem optiunea add it at one of your albums.

--- Controllers/AlbumsController.cs
+++ Controllers/AlbumsController.cs
@@ -29,5 +29,28 @@ namespace ArtX.Controllers
 
         }
 
+        public ActionResult New()
+        {
+        /*    var bookmarks = from b in db.Bookmarks    //daca vom face sa si selecteze bookmarksuri inca de la creearea albumului, dar eu zic ca nu e nevoie
+                         select b;
+            ViewBag.Bookmarks = bookmarks;*/
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult New(Album album)
+        {
+            try
+            {
+                db.Albums.Add(album);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch (Exception e)
+            {
+                return View();
+            }
+        }
+
     }
 }
