cf3fa09da6abf9395704a68d778e49d44d3e7a6a
Iulia3 <iulia_andreea9@yahoo.com>
2020-12-20 02:11:19 +0200

    UnsignedUser repaired
    Aparent faza cu utilizatorul neinregistrat nu mergea. Merge acum.

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace ArtX
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Bookmarks", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
