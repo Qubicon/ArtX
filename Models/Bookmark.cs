67f0a77dec7654403d6b45446ad76aad3c0869fb
Iulia3 <iulia_andreea9@yahoo.com>
2021-01-01 19:46:53 +0200

    Tag

--- Models/Bookmark.cs
+++ Models/Bookmark.cs
@@ -26,22 +26,19 @@ namespace ArtX.Models
         public int Rating { get; set; } // (Asta cumva ar trebui sa fie initializat cu 0 din db - momentan il afisam ca number) Vreau sa-l fac cu stelute, dar va fi mai complicat si nu stiu daca avem timp pentru asta
         public DateTime Date { get; set; }
 
-
+        public string Tags { get; set; }
 
         public string UserId { get; set; }
 
         public virtual ApplicationUser User { get; set; }
 
-
         [Required(ErrorMessage = "Albumul este obligatoriu")]
         public int AlbumId { get; set; } // FK
 
-        public virtual Album Album { get; set; }    //De tinut minte: pe asta va trebui sa il facem tot o lista. Un bookmark poate fi regasit in mai multe albume
-       // public virtual ICollection<Album> Albums { get; set; }
+        public virtual Album Album { get; set; }
 
         public virtual ICollection<Comment> Comments { get; set; }
 
         public IEnumerable<SelectListItem> Alb { get; set; }
-
     }
 }
